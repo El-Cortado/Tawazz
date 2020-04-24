6bba9a56c62e4bebf48972092bd7a321caaa8b96
tirza <tirza98@gmail.com>
2020-04-24 13:44:09 +0300

    rename FailedWaitingForCondition > FailedWaitingForConditionException

---
+++ app/src/main/java/com/example/tawazz/utils/exceptions/FailedWaitingForConditionException.java
@@ -0,0 +1,11 @@
+package com.example.tawazz.utils.exceptions;
+
+public class FailedWaitingForConditionException extends Exception {
+    public FailedWaitingForConditionException() {
+        super();
+    }
+
+    public FailedWaitingForConditionException(Throwable cause) {
+        super(cause);
+    }
+}
